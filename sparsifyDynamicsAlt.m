d1655d536b752aafeb6d2ebf53dc3753a1a2783e
Jason Bramburger <68243199+jbramburger@users.noreply.github.com>
2020-07-13 15:14:52 -0400

    Add files via upload

--- sparsifyDynamicsAlt.m
+++ sparsifyDynamicsAlt.m
@@ -1,6 +1,5 @@
-function Xi = sparsifyDynamics2(Theta,dXdt,lambda,n)
-% Copyright 2015, All Rights Reserved
-% Code by Steven L. Brunton
+function Xi = sparsifyDynamicsAlt(Theta,dXdt,lambda,n)
+% Augmentation of the code by Steven L. Brunton
 % For Paper, "Discovering Governing Equations from Data: 
 %        Sparse Identification of Nonlinear Dynamical Systems"
 % by S. L. Brunton, J. L. Proctor, and J. N. Kutz
